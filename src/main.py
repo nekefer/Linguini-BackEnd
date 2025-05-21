20b448eef2920f5fd4be8c0a2cfa117311d5191f
nekefer <kloutsedaniel1@gmail.com>
2025-05-21 15:55:27 -0500

    first commit

---
+++ src/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from .database.core import engine, Base
+from .entities.user import User  # Import models to register them
+from .api import register_routes
+from .logging import configure_logging, LogLevels
+from fastapi.middleware.cors import CORSMiddleware
+
+
+configure_logging(LogLevels.info)
+
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # React dev server
+    "http://127.0.0.1:5173",
+    # Add production domain(s) here when deploying
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+
+""" Only uncomment below to create new tables, 
+otherwise the tests will fail if not connected
+"""
+Base.metadata.create_all(bind=engine)
+
+register_routes(app) 
